be706dbc923312542a7598be9db83f0f0f90eaea
BrunogGallo <brunogiorgiogallo@gmail.com>
2026-06-23 11:05:41 -0300

    ajusto tiempo de since_updated

--- main.py
+++ main.py
@@ -19,9 +19,13 @@ ms_client = MintsoftOrderClient()
 try:
     
     status_id = 20
-    now = datetime.now().replace(minute=(datetime.now().minute // 15) * 15, second=0, microsecond=0)
-    since_updated = now - timedelta(minutes=15)
+    if datetime.now().minute < 30: # Si es la 1er ejecucion de la hora, 00 y 15
+        now = datetime.now().replace(minute=0, second=0, microsecond=0)
+    else: # Para la 2da, 30 y 45
+        now = datetime.now().replace(minute=30, second=0, microsecond=0)
 
+    since_updated = now + timedelta(minutes = 60) # Agrego 1h para que se ajuste al horario de Mintsoft
+    
     print(f"Consultando órdenes que hayan sido packeadas desde {since_updated}")
     clients = ms_client.get_clients()
     new_packed_orders = ms_client.get_orders(since_updated, status_id)
